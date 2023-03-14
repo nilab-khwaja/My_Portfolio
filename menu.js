14807c103fdf09499d5e0a1838617b5d07eaec71
nilab-khwaja <nilab.khwajazada1212@gmail.com>
2023-03-14 15:21:41 +0430

    add linter and remove errors

--- menu.js
+++ menu.js
@@ -1,18 +1,14 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".menu");
-
-hamburger.addEventListener("click", mobileMenu);
-
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.menu');
 function mobileMenu() {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
 }
-
-const navLink = document.querySelectorAll(".nav-link");
-
-navLink.forEach(n => n.addEventListener("click", closeMenu));
-
+hamburger.addEventListener('click', mobileMenu);
 function closeMenu() {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
 }
+const navLink = document.querySelectorAll('.nav-link');
+
+navLink.forEach((n) => n.addEventListener('click', closeMenu));
