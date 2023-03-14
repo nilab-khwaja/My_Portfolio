ebfdf4f5d2eb89e8a29cba9cc5a38411d77f8d07
nilab-khwaja <nilab.khwajazada1212@gmail.com>
2023-03-14 12:23:43 +0430

    add js to the portfolio

--- menu.js
+++ menu.js
@@ -1,13 +1,18 @@
-const hamburger = document.querySelector('hamburger');
-const navMenu = document.querySelector('.menu');
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".menu");
 
-hamburger.addEventListener('click', () => {
-  hamburger.classList.toggle('active');
-  navMenu.classList.toggle('active');
-});
+hamburger.addEventListener("click", mobileMenu);
 
-document.querySelectorAll('.nav-link').foreach((n) => n
-  .addEventListener('click', () => {
-    hamburger.classList.remove('active');
-    navMenu.classList.remove('active');
-  }));
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
