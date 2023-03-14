1a2b8071ea00cbaac2d6f8b6905ada9f35e1e675
nilab-khwaja <nilab.khwajazada1212@gmail.com>
2023-03-14 20:06:55 +0430

    modify js file

--- menu.js
+++ menu.js
@@ -5,10 +5,11 @@ function mobileMenu() {
   navMenu.classList.toggle('active');
 }
 hamburger.addEventListener('click', mobileMenu);
+
+const navLink = document.querySelectorAll('.nav-link');
+
 function closeMenu() {
   hamburger.classList.remove('active');
   navMenu.classList.remove('active');
 }
-const navLink = document.querySelectorAll('.nav-link');
-
 navLink.forEach((n) => n.addEventListener('click', closeMenu));
