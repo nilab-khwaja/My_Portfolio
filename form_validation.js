52835943d09023d1f0027494aea88bca7434bfc0
nilab-khwaja <nilab.khwajazada1212@gmail.com>
2023-03-16 23:12:08 +0430

    create a js file for form validation

---
+++ form_validation.js
@@ -0,0 +1,13 @@
+const form = document.getElementById('form');
+const email = document.getElementById('email');
+const error = document.getElementById('error-msg');
+
+error.style.display = 'none';
+
+form.addEventListener ('submit' , (e) => {
+if(email.value !== email.value.toLowerCase()) {
+    e.preventDefault();
+    error.style.display = 'block';
+    error.innerHTML = 'please be sure that you entered correct email';
+}
+});
