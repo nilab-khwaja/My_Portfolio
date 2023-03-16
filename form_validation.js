caf2b7eac97144beed8322d8e9bb985f3af5120b
nilab-khwaja <nilab.khwajazada1212@gmail.com>
2023-03-16 23:53:24 +0430

    remove linters error

--- form_validation.js
+++ form_validation.js
@@ -4,10 +4,10 @@ const error = document.getElementById('error-msg');
 
 error.style.display = 'none';
 
-form.addEventListener ('submit' , (e) => {
-if(email.value !== email.value.toLowerCase()) {
+form.addEventListener('submit', (e) => {
+  if (email.value !== email.value.toLowerCase()) {
     e.preventDefault();
     error.style.display = 'block';
     error.innerHTML = 'please be sure that you entered correct email';
-}
+  }
 });
